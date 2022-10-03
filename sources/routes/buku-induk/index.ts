b1f72d781671b7f9d5300f991d4a0d8dc7cf6567
NotHydra <rizky.irswanda115@gmail.com>
2022-10-03 22:10:56 +0800

    feat: nav active global variable

--- sources/routes/buku-induk/index.ts
+++ sources/routes/buku-induk/index.ts
@@ -5,5 +5,6 @@ import { bukuIndukSiswaRouter } from "./siswa";
 export const bukuIndukRouter = Router();
 export const headTitle = "Buku Induk";
 export const partialPath = "./../../..";
+export const navActive = [1, 0];
 
 bukuIndukRouter.use("/siswa", bukuIndukSiswaRouter);
