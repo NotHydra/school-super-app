8528457214cb75f80ca13a9a51a09d8c2e9d730a
NotHydra <rizky.irswanda115@gmail.com>
2022-10-04 10:59:35 +0800

    feat: card item in index

--- sources/index.ts
+++ sources/index.ts
@@ -5,6 +5,7 @@ import { mongoDBURI, navItemArray } from "./depedency";
 import { localMoment } from "./utility";
 
 import { bukuIndukRouter } from "./routes/buku-induk";
+import { Siswa } from "./models";
 
 const app: Express = express();
 const port: number = 3000;
@@ -19,8 +20,38 @@ app.set("view engine", "ejs");
 app.set("views", "sources/views");
 app.use(express.static("sources/public"));
 
-app.get("/", (req, res) => {
-    res.render("pages/index", { headTitle, partialPath, navActive });
+app.get("/", async (req, res) => {
+    res.render("pages/index", {
+        headTitle,
+        partialPath,
+        navActive,
+        boxItemArray: [
+            {
+                id: 1,
+                title: "Siswa",
+                icon: "user",
+                value: await Siswa.countDocuments(),
+            },
+            {
+                id: 2,
+                title: "Alumni",
+                icon: "user",
+                value: await Siswa.countDocuments(),
+            },
+            {
+                id: 3,
+                title: "Guru",
+                icon: "user",
+                value: await Siswa.countDocuments(),
+            },
+            {
+                id: 4,
+                title: "Buku",
+                icon: "user",
+                value: await Siswa.countDocuments(),
+            },
+        ],
+    });
 });
 
 app.use("/buku-induk", bukuIndukRouter);
