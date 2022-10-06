401682ee81a1d6d897819dfc812e6dcbe822755b
NotHydra <rizky.irswanda115@gmail.com>
2022-10-06 22:27:53 +0800

    feat: read json

--- sources/utility.ts
+++ sources/utility.ts
@@ -1,4 +1,9 @@
 import moment from "moment";
+import fs from "fs";
 
 export const localMoment = moment;
 localMoment.locale("id");
+
+export function readJSON(path: string) {
+    return JSON.parse(fs.readFileSync(path, "utf-8"));
+}
