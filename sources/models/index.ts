9cf7a21f12d7fd78b0ef4a0368adc06e3f4f522b
NotHydra <rizky.irswanda115@gmail.com>
2022-10-06 22:31:25 +0800

    feat: mata pelajaran schema

--- sources/models/index.ts
+++ sources/models/index.ts
@@ -6,6 +6,7 @@ import { tahunMasukSchema } from "./buku-induk/tahun-masuk";
 import { tingkatSchema } from "./buku-induk/tingkat";
 import { jurusanSchema } from "./buku-induk/jurusan";
 import { rombelSchema } from "./buku-induk/rombel";
+import { mataPelajaranSchema } from "./penilaian/mata-pelajaran";
 
 export const Siswa = mongoose.model("siswa", siswaSchema, "siswa");
 export const TempatLahir = mongoose.model("tempat_lahir", tempatLahirSchema, "tempat_lahir");
@@ -14,3 +15,5 @@ export const TahunMasuk = mongoose.model("tahun_masuk", tahunMasukSchema, "tahun
 export const Tingkat = mongoose.model("tingkat", tingkatSchema, "tingkat");
 export const Jurusan = mongoose.model("jurusan", jurusanSchema, "jurusan");
 export const Rombel = mongoose.model("rombel", rombelSchema, "rombel");
+
+export const MataPelajaran = mongoose.model("mata_pelajaran", mataPelajaranSchema, "mata_pelajaran");
