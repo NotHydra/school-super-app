0f1bca4eb02ae59b0b42192ee7948716409eefcb
NotHydra <rizky.irswanda115@gmail.com>
2022-10-09 00:55:38 +0800

    feat: jenis kelamin

--- scripts/python/main.py
+++ scripts/python/main.py
@@ -21,10 +21,15 @@ class Dependency:
             "scripts/json/dependency/data-umum/tempat_lahir.json"
         )
 
+        jenisKelaminArray = Utility.readJSON(
+            "scripts/json/dependency/data-umum/jenis_kelamin.json"
+        )
+
 
 class DataUmum:
     def main():
         DataUmum.tempatLahir()
+        DataUmum.jenisKelamin()
 
     def tempatLahir():
         tempatLahirArray = []
@@ -41,6 +46,24 @@ class DataUmum:
             tempatLahirArray.append(tempatLahirObject)
 
         Utility.writeJSON("scripts/json/data-umum/tempat_lahir.json", tempatLahirArray)
+
+    def jenisKelamin():
+        jenisKelaminArray = []
+        for jenisKelaminIndex, jenisKelamin in enumerate(
+            Dependency.DataUmum.jenisKelaminArray
+        ):
+            jenisKelaminObject = {
+                "_id": jenisKelaminIndex + 1,
+                "jenis_kelamin": jenisKelamin,
+                "dibuat": {"$date": {"$numberLong": Utility.currentDate()}},
+                "diubah": {"$date": {"$numberLong": Utility.currentDate()}},
+            }
+
+            jenisKelaminArray.append(jenisKelaminObject)
+
+        Utility.writeJSON(
+            "scripts/json/data-umum/jenis_kelamin.json", jenisKelaminArray
+        )
 
 
 class Main:
