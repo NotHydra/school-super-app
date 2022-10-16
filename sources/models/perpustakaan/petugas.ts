9f5c621496f953c9784f96556690e7f02c12abfc
NotHydra <rizky.irswanda115@gmail.com>
2022-10-16 12:01:54 +0800

    feat: petugas schema

---
+++ sources/models/perpustakaan/petugas.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+export const petugasSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true,
+    },
+    nama: {
+        type: String,
+        required: true,
+    },
+    id_tempat_lahir: {
+        type: Number,
+        required: true,
+        ref: "tempat_lahir",
+    },
+    tanggal_lahir: {
+        type: Date,
+        required: true,
+    },
+    id_jenis_kelamin: {
+        type: Number,
+        required: true,
+        ref: "jenis_kelamin",
+    },
+    jabatan: {
+        type: String,
+        required: true,
+    },
+    nomor_telepon: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    dibuat: {
+        type: Date,
+        required: true,
+    },
+    diubah: {
+        type: Date,
+        required: true,
+    },
+});
