431766f0a3b3cbc22851ee0cfc1bfe962a22a29a
NotHydra <rizky.irswanda115@gmail.com>
2022-10-23 23:26:36 +0800

    feat: pengajar line chart

--- sources/routes/pengajar/index.ts
+++ sources/routes/pengajar/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import { datasetYear } from "../../utility";
+
+import { Guru, Jabatan } from "../../models";
+
 import { pengajarGuruRouter } from "./guru";
 import { pengajarJabatanRouter } from "./jabatan";
 
@@ -8,10 +12,66 @@ export const headTitle = "Pengajar";
 const navActive = [1, 0];
 
 pengajarRouter.get("/", async (req, res) => {
+    const currentYear = new Date().getFullYear();
+
+    const guruChartData: any = await datasetYear(Guru, currentYear);
+    const jabatanChartData: any = await datasetYear(Jabatan, currentYear);
+
     res.render("pages/index", {
         headTitle,
         navActive,
-        cardItemArray: [],
+        cardItemArray: [
+            {
+                id: 1,
+                cardItemChild: [
+                    {
+                        id: 1,
+                        title: "Guru",
+                        icon: "user-tie",
+                        value: await Guru.countDocuments().lean(),
+                        link: "pengajar/guru",
+                    },
+                    {
+                        id: 2,
+                        title: "Jabatan",
+                        icon: "tag",
+                        value: await Jabatan.countDocuments().lean(),
+                        link: "pengajar/jabatan",
+                    },
+                ],
+            },
+        ],
+        lineChartArray: [
+            {
+                id: 1,
+                lineChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik Guru",
+                        link: { link: "pengajar/guru", title: "Guru", subTitle: "Pengajar" },
+                        value: guruChartData.currentYearValue,
+                        text: "Guru Baru",
+                        percentage: guruChartData.percentageIncrease,
+                        timeRange: "Sejak Tahun Lalu",
+                        dataset: guruChartData.dataset,
+                        firstLegend: "Tahun Ini",
+                        secondLegend: "Tahun Lalu",
+                    },
+                    {
+                        id: 2,
+                        title: "Statistik Jabatan",
+                        link: { link: "pengajar/jabatan", title: "Jabatan", subTitle: "Pengajar" },
+                        value: jabatanChartData.currentYearValue,
+                        text: "Jabatan Baru",
+                        percentage: jabatanChartData.percentageIncrease,
+                        timeRange: "Sejak Tahun Lalu",
+                        dataset: jabatanChartData.dataset,
+                        firstLegend: "Tahun Ini",
+                        secondLegend: "Tahun Lalu",
+                    },
+                ],
+            },
+        ],
     });
 });
 
