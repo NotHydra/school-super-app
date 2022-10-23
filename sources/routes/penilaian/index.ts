5935c3a748d0441cade9d9025f69f66266881b06
NotHydra <rizky.irswanda115@gmail.com>
2022-10-24 06:40:48 +0800

    feat: penilaian line chart

--- sources/routes/penilaian/index.ts
+++ sources/routes/penilaian/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import { datasetYear } from "../../utility";
+
+import { MataPelajaran } from "../../models";
+
 import { instansiMataPelajaranRouter } from "./mata-pelajaran";
 
 export const penilaianRouter = Router();
@@ -7,10 +11,46 @@ export const headTitle = "Penilaian";
 const navActive = [4, 0];
 
 penilaianRouter.get("/", async (req, res) => {
+    const currentYear = new Date().getFullYear();
+
+    const mataPelajaranChartData: any = await datasetYear(MataPelajaran, currentYear);
+
     res.render("pages/index", {
         headTitle,
         navActive,
-        cardItemArray: [],
+        cardItemArray: [
+            {
+                id: 1,
+                cardItemChild: [
+                    {
+                        id: 1,
+                        title: "Mata Pelajaran",
+                        icon: "book-bookmark",
+                        value: await MataPelajaran.countDocuments().lean(),
+                        link: "penilaian/mata-pelajaran",
+                    },
+                ],
+            },
+        ],
+        lineChartArray: [
+            {
+                id: 1,
+                lineChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik Mata Pelajaran Baru",
+                        link: { link: "penilaian/mata-pelajaran", title: "Mata Pelajaran", subTitle: "Penilaian" },
+                        value: mataPelajaranChartData.currentYearValue,
+                        text: "Mata Pelajaran Baru",
+                        percentage: mataPelajaranChartData.percentageIncrease,
+                        timeRange: "Sejak Tahun Lalu",
+                        dataset: mataPelajaranChartData.dataset,
+                        firstLegend: "Tahun Ini",
+                        secondLegend: "Tahun Lalu",
+                    },
+                ],
+            },
+        ],
     });
 });
 
