6248b258393afca17322ac91c27cbe8e5bd5f63d
NotHydra <rizky.irswanda115@gmail.com>
2022-10-24 11:29:15 +0800

    feat: pengajar donut chart

--- sources/routes/pengajar/index.ts
+++ sources/routes/pengajar/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-import { datasetYear } from "../../utility";
+import { blueColorPattern, datasetYear } from "../../utility";
 
-import { Guru, Jabatan } from "../../models";
+import { Guru, Jabatan, JenisKelamin, TempatLahir } from "../../models";
 
 import { pengajarGuruRouter } from "./guru";
 import { pengajarJabatanRouter } from "./jabatan";
@@ -16,6 +16,9 @@ pengajarRouter.get("/", async (req, res) => {
 
     const guruChartData: any = await datasetYear(Guru, currentYear);
     const jabatanChartData: any = await datasetYear(Jabatan, currentYear);
+
+    const jenisKelaminTotal = await JenisKelamin.countDocuments().lean();
+    const tempatLahirTotal = await TempatLahir.countDocuments().lean();
 
     res.render("pages/index", {
         headTitle,
@@ -68,6 +71,45 @@ pengajarRouter.get("/", async (req, res) => {
                         dataset: jabatanChartData.dataset,
                         firstLegend: "Tahun Ini",
                         secondLegend: "Tahun Lalu",
+                    },
+                ],
+            },
+        ],
+        donutChartArray: [
+            {
+                id: 1,
+                donutChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik Guru Berdasarkan Jenis Kelamin",
+                        dataset: await Promise.all(
+                            (
+                                await JenisKelamin.find().select("jenis_kelamin").sort({ jenis_kelamin: 1 }).lean()
+                            ).map(async (itemObject, itemIndex) => {
+                                return {
+                                    id: itemIndex + 1,
+                                    label: itemObject.jenis_kelamin,
+                                    value: await Guru.countDocuments({ id_jenis_kelamin: itemObject._id }).lean(),
+                                    color: blueColorPattern(itemIndex + 1, jenisKelaminTotal),
+                                };
+                            })
+                        ),
+                    },
+                    {
+                        id: 2,
+                        title: "Statistik Guru Berdasarkan Tempat Lahir",
+                        dataset: await Promise.all(
+                            (
+                                await TempatLahir.find().select("tempat_lahir").sort({ tempat_lahir: 1 }).lean()
+                            ).map(async (itemObject, itemIndex) => {
+                                return {
+                                    id: itemIndex + 1,
+                                    label: itemObject.tempat_lahir,
+                                    value: await Guru.countDocuments({ id_tempat_lahir: itemObject._id }).lean(),
+                                    color: blueColorPattern(itemIndex + 1, tempatLahirTotal),
+                                };
+                            })
+                        ),
                     },
                 ],
             },
