fe4141438ee7c8557bd29480f5c01deb1abed8b9
NotHydra <rizky.irswanda115@gmail.com>
2022-10-25 00:05:25 +0800

    feat: lulusan donut chart

--- sources/routes/lulusan/index.ts
+++ sources/routes/lulusan/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-import { datasetYear } from "../../utility";
+import { blueColorPattern, datasetYear } from "../../utility";
 
-import { Alumni, TahunLulus } from "../../models";
+import { Alumni, Rombel, Siswa, TahunLulus, TahunMasuk, TahunRombel } from "../../models";
 
 import { lulusanAlumniRouter } from "./alumni";
 import { lulusanTahunLulusRouter } from "./tahun-lulus";
@@ -16,6 +16,11 @@ lulusanRouter.get("/", async (req, res) => {
 
     const alumniChartData: any = await datasetYear(Alumni, currentYear);
     const tahunLulusChartData: any = await datasetYear(TahunLulus, currentYear);
+
+    const alumniDonutChartData: any = await Alumni.find().select("id_siswa").populate({ path: "id_siswa", select: "id_rombel id_tahun_masuk", model: Siswa }).lean();
+    const rombelTotal = await Rombel.countDocuments().lean();
+    const tahunMasukTotal = await TahunMasuk.countDocuments().lean();
+    const tahunLulusTotal = await TahunLulus.countDocuments().lean();
 
     res.render("pages/index", {
         headTitle,
@@ -68,6 +73,77 @@ lulusanRouter.get("/", async (req, res) => {
                         dataset: tahunLulusChartData.dataset,
                         firstLegend: "Tahun Ini",
                         secondLegend: "Tahun Lalu",
+                    },
+                ],
+            },
+        ],
+        donutChartArray: [
+            {
+                id: 1,
+                donutChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik Alumni Berdasarkan Rombel",
+                        dataset: await Promise.all(
+                            (
+                                await Rombel.find()
+                                    .select("rombel id_tahun_rombel")
+                                    .populate({ path: "id_tahun_rombel", select: "tahun_rombel", model: TahunRombel })
+                                    .sort({ rombel: 1 })
+                                    .lean()
+                            ).map(async (itemObject: any, itemIndex) => {
+                                let itemValue = 0;
+                                alumniDonutChartData.forEach((alumniDonutChartObject: any) => {
+                                    if (alumniDonutChartObject.id_siswa.id_rombel == itemObject._id) {
+                                        itemValue += 1;
+                                    }
+                                });
+                                return {
+                                    id: itemIndex + 1,
+                                    label: `${itemObject.rombel} - ${itemObject.id_tahun_rombel.tahun_rombel}`,
+                                    value: itemValue,
+                                    color: blueColorPattern(itemIndex + 1, rombelTotal),
+                                };
+                            })
+                        ),
+                    },
+                    {
+                        id: 2,
+                        title: "Statistik Alumni Berdasarkan Tahun Masuk",
+                        dataset: await Promise.all(
+                            (
+                                await TahunMasuk.find().select("tahun_masuk").sort({ tahun_masuk: 1 }).lean()
+                            ).map(async (itemObject: any, itemIndex) => {
+                                let itemValue = 0;
+                                alumniDonutChartData.forEach((alumniDonutChartObject: any) => {
+                                    if (alumniDonutChartObject.id_siswa.id_tahun_masuk == itemObject._id) {
+                                        itemValue += 1;
+                                    }
+                                });
+                                return {
+                                    id: itemIndex + 1,
+                                    label: itemObject.tahun_masuk,
+                                    value: itemValue,
+                                    color: blueColorPattern(itemIndex + 1, tahunMasukTotal),
+                                };
+                            })
+                        ),
+                    },
+                    {
+                        id: 3,
+                        title: "Statistik Alumni Berdasarkan Tahun Lulus",
+                        dataset: await Promise.all(
+                            (
+                                await TahunLulus.find().select("tahun_lulus").sort({ tahun_lulus: 1 }).lean()
+                            ).map(async (itemObject, itemIndex) => {
+                                return {
+                                    id: itemIndex + 1,
+                                    label: itemObject.tahun_lulus,
+                                    value: await Alumni.countDocuments({ id_tahun_lulus: itemObject._id }).lean(),
+                                    color: blueColorPattern(itemIndex + 1, tahunLulusTotal),
+                                };
+                            })
+                        ),
                     },
                 ],
             },
