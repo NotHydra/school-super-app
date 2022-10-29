4ac2162cbfe02b52bbc59c96383e908271963e46
NotHydra <rizky.irswanda115@gmail.com>
2022-10-29 20:19:49 +0800

    feat: login siswa

--- sources/authentication/index.ts
+++ sources/authentication/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { authenticationLoginRouter } from "./login";
+import { authenticationLoginSiswaRouter } from "./login-siswa";
 import { authenticationLogoutRouter } from "./logout";
 
 export const authenticationRouter = Router();
 
 authenticationRouter.use("/login", authenticationLoginRouter);
+authenticationRouter.use("/login-siswa", authenticationLoginSiswaRouter);
 authenticationRouter.use("/logout", authenticationLogoutRouter);
---
+++ sources/authentication/login-siswa.ts
@@ -0,0 +1,41 @@
+import express, { Router } from "express";
+import bcrypt from "bcrypt";
+
+import { isNotAuthenticated } from "../common/middleware/isNotAuthenticated";
+
+import { Siswa, User } from "../models";
+
+export const authenticationLoginSiswaRouter = Router();
+const headTitle = "Login Siswa";
+
+authenticationLoginSiswaRouter.use(express.static("sources/public"));
+authenticationLoginSiswaRouter.use(express.urlencoded({ extended: false }));
+
+authenticationLoginSiswaRouter.use(isNotAuthenticated);
+
+authenticationLoginSiswaRouter
+    .route("/")
+    .get(async (req, res) => {
+        res.render("pages/login-siswa", {
+            headTitle,
+            toastResponse: req.query.response,
+            toastTitle: req.query.title,
+            toastText: req.query.text,
+        });
+    })
+    .post(async (req, res) => {
+        const siswaObject = await Siswa.findOne({ nisn: req.body.nisn, tanggal_lahir: req.body.tanggal_lahir }).select("_id").lean();
+
+        if (siswaObject != null) {
+            req.session.regenerate(() => {
+                req.session.userId = siswaObject._id;
+                req.session.userType = "siswa";
+
+                req.session.save(() => {
+                    res.redirect("/?response=success");
+                });
+            });
+        } else if (siswaObject == null) {
+            res.redirect("/login-siswa?response=error&text=NISN atau tanggal lahir salah");
+        }
+    });
