9d95072a0c786cae94fda655a04bd679f832f5b5
NotHydra <rizky.irswanda115@gmail.com>
2022-10-29 15:49:01 +0800

    feat: user schema

--- sources/models/index.ts
+++ sources/models/index.ts
@@ -30,6 +30,8 @@ import { jenisKelaminSchema } from "./data-umum/jenis-kelamin";
 import { universitasSchema } from "./data-umum/universitas";
 import { pendidikanSchema } from "./data-umum/pendidikan";
 
+import { userSchema } from "./pengguna/user";
+
 const pengajarDatabase = mongoose.connection.useDb("pengajar");
 export const Guru = pengajarDatabase.model("guru", guruSchema, "guru");
 export const Jabatan = pengajarDatabase.model("jabatan", jabatanSchema, "jabatan");
@@ -69,3 +71,6 @@ export const TempatLahir = dataUmumDatabase.model("tempat_lahir", tempatLahirSch
 export const JenisKelamin = dataUmumDatabase.model("jenis_kelamin", jenisKelaminSchema, "jenis_kelamin");
 export const Universitas = dataUmumDatabase.model("universitas", universitasSchema, "universitas");
 export const Pendidikan = dataUmumDatabase.model("pendidikan", pendidikanSchema, "pendidikan");
+
+const penggunaDatabase = mongoose.connection.useDb("pengguna");
+export const User = penggunaDatabase.model("user", userSchema, "user");
---
+++ sources/models/pengguna/user.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    nama_lengkap: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    nomor_telepon: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    role: {
+        type: String,
+        enum: ["operator", "admin", "superadmin"],
+        required: true,
+    },
+    aktif: {
+        type: Boolean,
+        required: true,
+    },
+});
