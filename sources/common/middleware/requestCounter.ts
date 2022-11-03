685f5872203d7b2f94a19e526871f876417f4e74
NotHydra <rizky.irswanda115@gmail.com>
2022-11-03 21:19:55 +0800

    feat: request counter only for user

--- sources/common/middleware/requestCounter.ts
+++ sources/common/middleware/requestCounter.ts
@@ -3,24 +3,26 @@ import { NextFunction, Request, Response } from "express";
 import { Aktivitas } from "../../models";
 
 export async function requestCounter(req: Request, res: Response, next: NextFunction) {
-    res.on("finish", async () => {
-        if (!req.originalUrl.includes("dist") && !req.originalUrl.includes("plugins") && !req.originalUrl.includes("css")) {
-            const itemObject = new Aktivitas({
-                _id: (await Aktivitas.findOne().select("_id").sort({ _id: -1 }).lean())?._id + 1 || 1,
+    if (req.session.userType == "user") {
+        res.on("finish", async () => {
+            if (!req.originalUrl.includes("dist") && !req.originalUrl.includes("plugins") && !req.originalUrl.includes("css")) {
+                const itemObject = new Aktivitas({
+                    _id: (await Aktivitas.findOne().select("_id").sort({ _id: -1 }).lean())?._id + 1 || 1,
 
-                id_user: req.session.userId,
-                aktivitas: req.originalUrl,
-                method: req.method,
+                    id_user: req.session.userId,
+                    aktivitas: req.originalUrl,
+                    method: req.method,
 
-                dibuat: new Date(),
-                diubah: new Date(),
-            });
+                    dibuat: new Date(),
+                    diubah: new Date(),
+                });
 
-            try {
-                await itemObject.save();
-            } catch (error: any) {}
-        }
-    });
+                try {
+                    await itemObject.save();
+                } catch (error: any) {}
+            }
+        });
+    }
 
     next();
 }
